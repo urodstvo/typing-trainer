6cbf2f5874ee58dc70e786ea3cb2ed758d1ddd02
CLOWNAKIO <dead.kanji@mail.ru>
2024-08-10 21:35:41 +0300

    fix: setuped gh pages url base

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
+    base: '/typing-trainer/',
     test: {
         globals: true,
         environment: 'jsdom',
